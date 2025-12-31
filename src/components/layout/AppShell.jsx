29b925eb2190cd697d4dcb28ef2e8d5de8ce7870
veritasvoid <veritasvoid2025@gmail.com>
2025-12-31 08:34:54 -0700

    Update AppShell.jsx

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -4,8 +4,10 @@ import { BottomNav } from './BottomNav';
 
 export const AppShell = () => {
   return (
-    <div className="min-h-screen bg-background pb-16 md:pb-0">
-      <Outlet />
+    <div className="min-h-screen bg-background">
+      <div className="pb-20 md:pb-0">
+        <Outlet />
+      </div>
       <BottomNav />
     </div>
   );
