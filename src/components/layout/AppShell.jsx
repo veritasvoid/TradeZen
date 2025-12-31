5b9a118de4154d778b085319b46f3aa498cc3888
veritasvoid <veritasvoid2025@gmail.com>
2025-12-31 08:40:10 -0700

    Update AppShell.jsx

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { BottomNav } from './BottomNav';
+import { TopNav } from './TopNav';
 
 export const AppShell = () => {
   return (
     <div className="min-h-screen bg-background">
-      <div className="pb-20 md:pb-0">
-        <Outlet />
-      </div>
-      <BottomNav />
+      <TopNav />
+      <Outlet />
     </div>
   );
 };
