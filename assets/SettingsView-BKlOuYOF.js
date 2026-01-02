b20d189f38316454449d171050faebf289563f64
veritasvoid <veritasvoid@users.noreply.github.com>
2026-01-02 14:39:39 +0000

    deploy: fec16cfef47300f9d09e528d3dc9f8459b942070

--- assets/SettingsView-BKlOuYOF.js
+++ assets/SettingsView-BKlOuYOF.js
@@ -1,4 +1,4 @@
-import{d as N,a as I,u as z,j as e,f as E,r as f,C as L,S as w}from"./index-aHhHVrA3.js";import{a as M,u as R}from"./settingsStore-DAdnsdTg.js";import{T as y}from"./trash-2-Br4oKsR3.js";import"./useMutation-Xce8QMPk.js";/**
+import{d as N,a as I,u as z,j as e,f as E,r as f,C as L,S as w}from"./index-DEl1Q-JK.js";import{a as M,u as R}from"./settingsStore-jMawSk5y.js";import{T as y}from"./trash-2-CYvOcEsi.js";import"./useMutation-DCOQUJK-.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/plus-Byp2qooK.js
+++ assets/plus-Byp2qooK.js
@@ -1,4 +1,4 @@
-import{d as e}from"./index-aHhHVrA3.js";/**
+import{d as e}from"./index-DEl1Q-JK.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
