4477504bd691a17fa9cbc5f146bc97a4ef9b395d
veritasvoid <veritasvoid@users.noreply.github.com>
2026-01-10 19:37:12 +0000

    deploy: c53cd3e18181bc704adaf8da3ed958f23536d23a

--- assets/SettingsView-BPUcEj6v.js
+++ assets/SettingsView-BPUcEj6v.js
@@ -1,4 +1,4 @@
-import{f as N,a as I,u as z,j as e,n as E,d as L,k as M,r as b,C as R,S as y}from"./index-BphPB1Ha.js";import{u as Y}from"./useTrades-71k3FSB_.js";import{T as j}from"./trash-2-CCIk-7r_.js";/**
+import{f as N,a as I,u as z,j as e,n as E,d as L,k as M,r as b,C as R,S as y}from"./index-4exFXFou.js";import{u as Y}from"./useTrades-BH0AIRXc.js";import{T as j}from"./trash-2-BJWxS1NS.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
